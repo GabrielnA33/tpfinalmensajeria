e759e89164cd694ba59a709176284daa77cd6582
Gabriel Alvarez <alvarezgabrielnicolas@gmail.com>
2024-12-02 23:52:40 -0300

    Actualización responsiva

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ContactList from './ContactList/ContactList';
+import ContactList from './ContactList/ContactList'; 
 import './ChatList.css';
 
 const users = [
